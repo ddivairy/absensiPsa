58ffae22329456c70388721a789f42b6864c2259
Rasyaseptian <rasyaseptian999@gmail.com>
2026-09-25 08:34:37 +0700

    ngubah lokasi di bandung

--- src/components/Settings/SettingsView.tsx
+++ src/components/Settings/SettingsView.tsx
@@ -8,7 +8,7 @@ export const SettingsView: React.FC = () => {
   const [startTime, setStartTime] = useState(settings.startTime);
   const [lateLimitTime, setLateLimitTime] = useState(settings.lateLimitTime);
   const [endTime, setEndTime] = useState(settings.endTime);
-  const [locationName, setLocationName] = useState(settings.officeLocation.name || 'Punya Skill Akademi, Bandung');
+  const [locationName, setLocationName] = useState(settings.officeLocation.name || 'Punya Skill Akademi, Bandung, Jawa Barat');
   const [radiusMeters, setRadiusMeters] = useState(settings.officeLocation.radiusMeters);
   const [savedSuccess, setSavedSuccess] = useState(false);
 
